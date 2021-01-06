0a8e3acd5aa0c8d60f4d78558702c453533b068e
Emerson Ferreira <emersonfdasilva2@gmail.com>
2021-01-05 23:01:56 -0300

    01 RestWithSpringBootUdemy Scaffold

---
+++ 01 RestWithSpringBootUdemy Scaffold/src/main/java/io/github/emfsilva/Startup.java
@@ -0,0 +1,12 @@
+package io.github.emfsilva;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class Startup {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Startup.class, args);
+	}
+}
